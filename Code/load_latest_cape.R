12501cb6691d9bda00d84bd53a8c2efde991c78a
annatyler <anna.l.tyler@gmail.com>
2022-04-19 13:27:10 -0400

    Update load_latest_cape.R

--- Code/load_latest_cape.R
+++ Code/load_latest_cape.R
@@ -1,8 +1,8 @@
 #This function loads a local version of cape code.
+#load_libraries must already be loaded
 
 load_latest_cape <- function(cape.dir, personal.library = FALSE){
 
-    source(here("code", "load_libraries.R"))
     needed.libraries <- c("here", "qtl2", "abind", "Matrix", "MASS", "regress", "igraph",
         "RColorBrewer", "R6", "yaml", "tools", "caTools", "propagate", "igraph",
         "qtl", "regress", "evd")
