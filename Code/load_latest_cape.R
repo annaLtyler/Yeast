c7631e7e96cb8227ee1effcea9f7f65a16c608fd
annatyler <anna.l.tyler@gmail.com>
2022-04-21 13:34:47 -0400

    Update load_latest_cape.R

--- Code/load_latest_cape.R
+++ Code/load_latest_cape.R
@@ -2,7 +2,6 @@
 
 load_latest_cape <- function(cape.dir, personal.library = FALSE){
 
-    source(here("code", "load_libraries.R"))
     needed.libraries <- c("here", "qtl2", "abind", "Matrix", "MASS", "regress", "igraph",
         "RColorBrewer", "R6", "yaml", "tools", "caTools", "propagate", "igraph",
         "qtl", "regress", "evd", "shape")
