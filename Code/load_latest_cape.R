72c7c1e7dab2e57543041fca57f157e5d90a40ea
annatyler <anna.l.tyler@gmail.com>
2022-04-22 11:00:22 -0400

    added pracma to load_cape

--- Code/load_latest_cape.R
+++ Code/load_latest_cape.R
@@ -4,7 +4,7 @@ load_latest_cape <- function(cape.dir, personal.library = FALSE){
 
     needed.libraries <- c("here", "qtl2", "abind", "Matrix", "MASS", "regress", "igraph",
         "RColorBrewer", "R6", "yaml", "tools", "caTools", "propagate", "igraph",
-        "qtl", "regress", "evd", "shape")
+        "qtl", "regress", "evd", "shape", "pracma")
     load_libraries(needed.libraries, personal.library = personal.library)
 
     cape.fun <- list.files(file.path(cape.dir, "R"), pattern = ".R", full.names = TRUE)
